b854d7fe47f3e2d2ee0cd6a93c92c72af9fd96af
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-12-30 12:27:03 -0500

    rename

--- compile_and_clean.sh
+++ compile_and_clean.sh
@@ -4,7 +4,7 @@
 compile() {
     echo "Compiling all .c and .cpp files in the current folder and specified subfolders..."
     # Define an array of directories to search
-    directories=( "." "./hello-world-in-openmp" "./mpi" "./solutions" )
+    directories=( "." "./hello-world-in-openmp" "./mpi-examples" "./openmp-solutions" )
 
     for dir in "${directories[@]}"; do
         find "$dir" -type f \( -name "*.c" -o -name "*.cpp" \) | while read -r file; do
@@ -22,7 +22,7 @@ compile() {
                 mpicc -fopenmp -Wall -O2 -o "$output" "$file" "./random.c" -I"$dirname" || { echo "Failed to compile $file"; exit 1; }
             elif [[ $file == */pi_mc.c ]]; then
                 # Special case for pi_mc.c - compile with random_seq_lcg.c
-                mpicc -fopenmp -Wall -O2 -c "./solutions/random_seq_lcg.c" -o random_seq_lcg.o
+                mpicc -fopenmp -Wall -O2 -c "./openmp-solutions/random_seq_lcg.c" -o random_seq_lcg.o
                 mpicc -fopenmp -Wall -O2 -o "$output" "$file" random_seq_lcg.o || { echo "Failed to compile $file"; exit 1; }
                 rm -f random_seq_lcg.o
             elif [[ $file == *.c ]]; then
@@ -38,7 +38,7 @@ compile() {
 # Function to clean all .out files in the current folder and specified subfolders
 clean() {
     echo "Cleaning up all .out files in the current folder and specified subfolders..."
-    directories=( "." "./hello-world-in-openmp" "./mpi" "./solutions" )
+    directories=( "." "./hello-world-in-openmp" "./mpi-examples" "./openmp-solutions" )
     for dir in "${directories[@]}"; do
         find "$dir" -type f -name "*.out" -exec rm -f {} \;
         find "$dir" -type f -name "*.o" -exec rm -f {} \;
